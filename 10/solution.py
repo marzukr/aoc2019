c3dc329319372a0e403eb6657ae85c2b6e8dcb99
Marzuk Rashid <marzukr@gmail.com>
2019-12-10 18:24:19 -0600

    10 part 2

--- 10/solution.py
+++ 10/solution.py
@@ -18,41 +18,16 @@ with open("input.txt","r") as file1:
     max_y = i - 1
 
 # part 1
-# max_detects = []
-# for i in range(0, len(asteroids)):
-#     can_see = {a:(True if a != asteroids[i] else False) for a in asteroids}
-#     directions = 0
-#     for key in can_see:
-#         if can_see[key] == False:
-#             continue
-
-#         j = 1
-#         to_test_i = (key[0] - asteroids[i][0], key[1] - asteroids[i][1])
-#         divisor = max(math.gcd(to_test_i[0], to_test_i[1]), 1)
-#         to_test_i = (to_test_i[0] // divisor, to_test_i[1] // divisor)
-
-#         candidate = asteroids[i]
-#         while candidate[0] <= max_x and candidate[1] <= max_y and candidate[0] >= 0 and candidate[1] >= 0:
-#             candidate = (asteroids[i][0] + to_test_i[0] * j, asteroids[i][1] + to_test_i[1] * j)
-#             if candidate in can_see:
-#                 can_see[candidate] = False
-#             j += 1
-#         directions += 1
-#     max_detects.append(directions)
-# print(max(max_detects))
-# print(max_detects.index(max(max_detects)))
-# print(asteroids[262])
-
-def laser_direction_gen(center):
-    edge = (center[0], 0)
-    direc = (edge[0] - )
-
+max_detects = []
+asteroid_lines = []
 laser_center = (29, 28)
-laser_edge = (29, 0)
+# laser_center = (11,13)
 for i in range(0, len(asteroids)):
     can_see = {a:(True if a != asteroids[i] else False) for a in asteroids}
     directions = 0
+    this_asteroid_line = []
     for key in can_see:
+        asteroids_in_line = []
         if can_see[key] == False:
             continue
 
@@ -66,6 +41,20 @@ for i in range(0, len(asteroids)):
             candidate = (asteroids[i][0] + to_test_i[0] * j, asteroids[i][1] + to_test_i[1] * j)
             if candidate in can_see:
                 can_see[candidate] = False
+                asteroids_in_line.append(candidate)
             j += 1
         directions += 1
+
+        theta = math.acos((to_test_i[1] * -1) / math.sqrt(to_test_i[0] ** 2 + to_test_i[1] ** 2))
+        if to_test_i[0] < 0:
+            theta = 2*math.pi - theta
+        this_asteroid_line.append((theta, asteroids_in_line))
+    if asteroids[i] == laser_center:
+        asteroid_lines = this_asteroid_line
     max_detects.append(directions)
+
+asteroid_lines = sorted(asteroid_lines)
+j = 0
+for i in range(0, 200):
+    print(asteroid_lines[j % len(asteroid_lines)][1].pop(0))
+    j += 1
